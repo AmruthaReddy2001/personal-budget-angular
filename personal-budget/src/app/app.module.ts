a8e3e236212e9b6b00c42a80e896ebee3c825511
AmruthaReddy2001 <nagaamruthareddy99@gmail.com>
2024-02-10 12:43:49 -0500

    Added Angular

---
+++ personal-budget/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArticleComponent } from './article/article.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { P404Component } from './p404/p404.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ArticleComponent,
+    FooterComponent,
+    HeroComponent,
+    HomepageComponent,
+    LoginComponent,
+    MenuComponent,
+    P404Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
